f3d694d7322eb3dd1ec568871b82c5f631628081
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-08-04 04:01:22 +0900

    feat: impl logic

--- at_coder/card_game_for_two/src/main.rs
+++ at_coder/card_game_for_two/src/main.rs
@@ -14,5 +14,21 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let n = read();
+    let mut a: Vec<u32> = (0..n).map(|_| read()).collect();
+
+    a.sort_by(|x,y| x.cmp(y).reverse());
+
+    let mut alice = 0;
+    let mut bob = 0;
+
+    for (i,&x) in a.iter().enumerate() {
+        if i % 2 == 0 {
+            alice += x;
+        } else {
+            bob += x;
+        }
+    }
+
+    println!("{}",alice - bob);
 }
