ebe8bb54155870613084a9decfa653ac91c602c5
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-08-04 03:28:38 +0900

    feat: impl logic

--- at_coder/coins/src/main.rs
+++ at_coder/coins/src/main.rs
@@ -14,5 +14,22 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let a: u32 = read();
+    let b: u32 = read();
+    let c: u32 = read();
+    let x: u32 = read();
+
+    let mut ans = 0;
+
+    for i in 0..a + 1 {
+        for j in 0..b + 1 {
+            for k in 0..c + 1 {
+                if 500 * i + 100 * j + 50 * k == x {
+                    ans += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", ans)
 }
