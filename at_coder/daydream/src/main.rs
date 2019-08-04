c7cd13e8e2ece4ac0050466d1a5299be870cc58e
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-08-05 03:07:30 +0900

    feat: impl logic

--- at_coder/daydream/src/main.rs
+++ at_coder/daydream/src/main.rs
@@ -14,5 +14,23 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let patterns: Vec<Vec<char>> = ["dream", "dreamer", "erase", "eraser"]
+        .iter()
+        .map(|s| s.chars().rev().collect())
+        .collect();
+    let s: Vec<char> = read::<String>().chars().rev().collect();
+    let mut s = &s[..];
+    let mut succeeded = true;
+
+    while s.len() > 0 {
+        let matched = patterns.iter().find(|&p| s.starts_with(p));
+        if let Some(p) = matched {
+            s = &s[p.len()..];
+        } else {
+            succeeded = false;
+            break;
+        }
+    }
+
+    println!("{}", if succeeded { "YES" } else { "NO" });
 }
