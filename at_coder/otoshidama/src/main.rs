c9ca379536ce54fee9bb550276d7cedac3f867e5
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-08-05 02:38:50 +0900

    feat: impl logic

--- at_coder/otoshidama/src/main.rs
+++ at_coder/otoshidama/src/main.rs
@@ -14,5 +14,20 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let n: i32 = read();
+    let x: i32 = read();
+    let mut ans = None;
+
+    'outer: for i in 0..n + 1 {
+        for j in 0..n + 1 {
+            let k = n - i - j;
+            if i * 10000 + j * 5000 + k * 1000 == x {
+                ans = Some((i, j, k));
+                break 'outer;
+            }
+        }
+    }
+
+    let (x, y, z) = ans.unwrap_or((-1, -1, -1));
+    println!("{} {} {}", x, y, z);
 }
