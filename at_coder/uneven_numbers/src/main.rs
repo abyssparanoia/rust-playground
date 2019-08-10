98e72fb02829d019f038ee481666902cec31b6e8
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-08-11 06:12:48 +0900

    feat: impl logic

--- at_coder/uneven_numbers/src/main.rs
+++ at_coder/uneven_numbers/src/main.rs
@@ -14,5 +14,38 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let n: i32 = read();
+
+    let mut num = 1;
+
+    loop {
+        let temp = 10i32.pow(num) as f32;
+        let result = (n as f32) / temp;
+        if result <= 1.0 {
+            break;
+        }
+        num += 1;
+    }
+
+    let mut ans = 0;
+
+    for i in 1..num + 1 {
+        if i == 1 {
+            ans += 9;
+            continue;
+        }
+
+        if i % 2 == 0 {
+            continue;
+        }
+
+        if i == num {
+            ans += n - 10i32.pow(i - 1) + 1;
+            continue;
+        }
+
+        ans += 10i32.pow(i) - 10i32.pow(i - 1) - 1;
+    }
+
+    println!("{}", ans)
 }
