6a1e83e130a4ba373da3d3011afd0b90eb2a517e
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-08-12 20:18:12 +0900

    feat: impl logic

--- at_coder/max_gcd/src/main.rs
+++ at_coder/max_gcd/src/main.rs
@@ -18,15 +18,40 @@ fn recurse<X, Y>(x: X, f: &Fn(X, &Fn(X) -> Y) -> Y) -> Y {
 }
 
 fn main() {
-    let a = 21;
-    let b = 14;
+    // let a = 21;
+    // let b = 14;
 
-    let ans = recurse((a, b), &|(x, y), gcd| {
-        if y == 0 {
-            return x;
+    // let ans = recurse((a, b), &|(x, y), gcd| {
+    //     if y == 0 {
+    //         return x;
+    //     }
+    //     gcd((y, x % y))
+    // });
+
+    let N: u32 = read();
+    let K: u32 = read();
+
+    let a_n: Vec<u32> = (0..N).map(|_| read()).collect();
+
+    let mut ans = 0;
+
+    for _ in 0..K {
+        for n in 0..a_n.len() {
+            let x: u32 = *a_n.get(n).unwrap();
+            let y: u32 = *a_n.get(n + 1).unwrap();
+
+            ans = recurse((x, y), &|(xt, yt), gcd| {
+                if yt == 0 {
+                    return xt;
+                }
+                gcd((yt, xt % yt))
+            });
         }
-        gcd((y, x % y))
-    });
+
+        if ans != 0 {
+            break;
+        }
+    }
 
     println!("{}", ans);
 }
