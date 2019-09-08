f7529759e06fdf0e12d4ebc82230cbbc80d8208d
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-09-09 02:38:49 +0900

    feat: impl logic

--- aizu_online_judge/greatest_common_divisor/src/main.rs
+++ aizu_online_judge/greatest_common_divisor/src/main.rs
@@ -1,3 +1,33 @@
+use std::io::*;
+use std::str::FromStr;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
+}
+
+fn gcd(x: u32, y: u32) -> u32 {
+    let r = x % y;
+
+    if r == 0 {
+        return y;
+    }
+
+    return gcd(y, r);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x: u32 = read();
+    let y: u32 = read();
+
+    let result = if x > y { gcd(x, y) } else { gcd(y, x) };
+
+    println!("{}", result)
 }
