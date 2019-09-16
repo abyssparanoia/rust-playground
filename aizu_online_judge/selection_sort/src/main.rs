e249d2300d2a32e201b3ce0cc6cbd0c2a02cee64
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-09-16 23:41:52 +0900

    feat: impl logic

--- aizu_online_judge/selection_sort/src/main.rs
+++ aizu_online_judge/selection_sort/src/main.rs
@@ -14,5 +14,33 @@ fn read<T: FromStr>() -> T {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let n: u32 = read();
+
+    let mut target_array: Vec<u32> = (0..n).map(|_| read::<u32>()).collect();
+    let mut count = 0;
+
+    for i in 0..n {
+        let mut min_j = i;
+        for j in i..n {
+            if target_array[j as usize] < target_array[min_j as usize] {
+                min_j = j
+            };
+        }
+        if min_j != i {
+            let temp = target_array[i as usize];
+            target_array[i as usize] = target_array[min_j as usize];
+            target_array[min_j as usize] = temp;
+            count += 1;
+        }
+    }
+
+    for index in 0..n {
+        if index != n - 1 {
+            print!("{} ", target_array[index as usize]);
+        } else {
+            print!("{}", target_array[index as usize]);
+        }
+    }
+    println!("");
+    println!("{}", count);
 }
