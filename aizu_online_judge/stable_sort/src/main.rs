e363fdf4cb945f3c2cecd0b5e81df01cc8b145f0
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-09-17 00:46:52 +0900

    feat: impl logic

--- aizu_online_judge/stable_sort/src/main.rs
+++ aizu_online_judge/stable_sort/src/main.rs
@@ -1,6 +1,12 @@
 use std::io::*;
 use std::str::FromStr;
 
+#[derive(Debug)]
+struct Card {
+    suit: char,
+    value: u32,
+}
+
 fn read<T: FromStr>() -> T {
     let stdin = stdin();
     let stdin = stdin.lock();
@@ -13,41 +19,63 @@ fn read<T: FromStr>() -> T {
     token.parse().ok().expect("failed to parse token")
 }
 
-fn bubble_sort(mut target_array: Vec<u32>) {
+fn bubble_sort(target_array: &mut Vec<Card>) {
     let n = target_array.len();
     let mut flag = true;
 
     while flag {
         flag = false;
         for j in (1..n).rev() {
-            if target_array[j as usize] < target_array[(j - 1) as usize] {
-                let temp = target_array[j as usize];
-                target_array[j as usize] = target_array[(j - 1) as usize];
-                target_array[(j - 1) as usize] = temp;
+            if target_array[j as usize].value < target_array[(j - 1) as usize].value {
+                target_array.swap(j as usize, (j - 1) as usize);
                 flag = true;
             }
         }
     }
 }
 
-fn selection_sort(mut target_array: Vec<u32>) {
+fn selection_sort(target_array: &mut Vec<Card>) {
     let n = target_array.len();
 
     for i in 0..n {
         let mut min_j = i;
         for j in i..n {
-            if target_array[j as usize] < target_array[min_j as usize] {
+            if target_array[j as usize].value < target_array[min_j as usize].value {
                 min_j = j
             };
         }
         if min_j != i {
-            let temp = target_array[i as usize];
-            target_array[i as usize] = target_array[min_j as usize];
-            target_array[min_j as usize] = temp;
+            target_array.swap(i as usize, min_j as usize);
         }
     }
 }
 
+fn display_array<T: std::fmt::Debug>(target_array: &mut Vec<T>) {
+    let n = target_array.len();
+
+    for index in 0..n {
+        if index != n - 1 {
+            print!("{:?}", target_array[index as usize]);
+        } else {
+            print!("{:?}", target_array[index as usize]);
+        }
+    }
+    println!("");
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let n: u32 = read();
+
+    let mut target_array: Vec<Card> = (0..n)
+        .map(|_| read::<String>())
+        .map(|input_v| Card {
+            suit: input_v.chars().nth(0).unwrap(),
+            value: input_v.chars().nth(1).unwrap() as u32,
+        })
+        .collect();
+
+    bubble_sort(&mut target_array);
+    display_array::<Card>(&mut target_array);
 }
