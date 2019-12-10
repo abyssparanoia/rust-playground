ef1d46b200fb78790a33d9c0193d6d630ad3bab3
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-11 02:41:40 +0900

    feat: impl inssertion sort

--- aizu_online_judge/shell_sort/src/main.rs
+++ aizu_online_judge/shell_sort/src/main.rs
@@ -13,6 +13,29 @@ fn read<T: FromStr>() -> T {
     token.parse().ok().expect("failed to parse token")
 }
 
+fn insertion_sort(mut input_array: Vec<i32>, n: i32, g: i32) -> i32 {
+    let mut cnt = 0;
+
+    for i in 0..n {
+        let v = input_array[i as usize];
+        let mut j = i - g;
+
+        while j >= 0 && input_array[j as usize] > v {
+            input_array[(j + g) as usize] = input_array[j as usize];
+            j = j - g;
+            cnt += 1;
+        }
+
+        input_array[(j + g) as usize] = v
+    }
+
+    cnt
+}
+
 fn main() {
+    let n: i32 = read();
+
+    let mut input_array: Vec<i32> = (0..n).map(|_| read::<i32>()).collect();
+
     println!("Hello, world!");
 }
