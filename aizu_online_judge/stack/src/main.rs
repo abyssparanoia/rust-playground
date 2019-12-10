b6dedcfd586571468d7546cf491529aa3868cc1c
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-11 07:36:38 +0900

    feat: impl stack

--- aizu_online_judge/stack/src/main.rs
+++ aizu_online_judge/stack/src/main.rs
@@ -13,7 +13,22 @@ fn read_line<T: FromStr>() -> Vec<T> {
 fn main() {
     let token_array = read_line::<char>();
 
+    let mut stack: Vec<i32> = vec![];
     for i in 0..token_array.len() {
-        println!("{}", token_array[i as usize])
+        let token = token_array[i as usize];
+        match token.to_digit(10) {
+            Some(token) => stack.push(token as i32),
+            _ => {
+                let x = stack.pop().unwrap();
+                let y = stack.pop().unwrap();
+                match &*token.to_string() {
+                    "+" => stack.push(y + x),
+                    "-" => stack.push(y - x),
+                    "*" => stack.push(y * x),
+                    _ => println!("failed"),
+                }
+            }
+        }
     }
+    println!("{}", stack[0])
 }
