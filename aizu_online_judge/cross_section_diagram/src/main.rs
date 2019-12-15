0aa9f2c205589bb003dccda2c91b976c1dabd5fd
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-15 17:08:44 +0900

    feat: added read line fun

--- aizu_online_judge/cross_section_diagram/src/main.rs
+++ aizu_online_judge/cross_section_diagram/src/main.rs
@@ -1,3 +1,15 @@
+use std::io::*;
+use std::str::FromStr;
+
+fn read_line<T: FromStr>() -> Vec<T> {
+    let mut s = String::new();
+    stdin().read_line(&mut s).ok();
+    s.trim()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
 fn main() {
     println!("Hello, world!");
 }
