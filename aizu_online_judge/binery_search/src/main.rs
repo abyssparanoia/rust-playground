8ce29901acc96856df328b2087d8a8947876ae6b
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-15 23:19:09 +0900

    feat: impl reader

--- aizu_online_judge/binery_search/src/main.rs
+++ aizu_online_judge/binery_search/src/main.rs
@@ -1,3 +1,21 @@
+use std::io::*;
+use std::str::FromStr;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n: usize = read();
+    let s_array: Vec<i32> = (0..n).map(|_| read::<i32>()).collect();
+    let q: usize = read();
+    let t_array: Vec<i32> = (0..q).map(|_| read::<i32>()).collect();
 }
