fce8d2eb0e3d90f8cae20d03e688ce97b3467725
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-15 22:12:46 +0900

    feat: impl logic

--- aizu_online_judge/cross_section_diagram/src/main.rs
+++ aizu_online_judge/cross_section_diagram/src/main.rs
@@ -1,22 +1,75 @@
 use std::io::*;
 use std::str::FromStr;
 
-fn read_line<T: FromStr>() -> Vec<T> {
-    let mut s = String::new();
-    stdin().read_line(&mut s).ok();
-    s.trim()
-        .split_whitespace()
-        .map(|e| e.parse().ok().unwrap())
-        .collect()
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
 }
 
-type Area = (isize, isize);
+type Area = (usize, usize);
 
 fn main() {
-    let token_list: Vec<char> = read_line();
+    let token_str: String = read();
 
-    let index_stack: Vec<i32> = vec![];
-    let area_stack: Vec<Area> = vec![];
+    let mut index_stack: Vec<usize> = vec![];
+    let mut area_stack: Vec<Area> = vec![];
 
-    println!("Hello, world!");
+    let mut sum: usize = 0;
+
+    for (i, token) in token_str.chars().enumerate() {
+        match &token.to_string()[..] {
+            "\\" => {
+                index_stack.push(i);
+            }
+            "/" => {
+                let j = index_stack.pop();
+                match j {
+                    Some(j) => {
+                        let mut a = i - j;
+                        sum += a;
+                        loop {
+                            let area_tuple = area_stack.pop();
+                            match area_tuple {
+                                Some(area) => {
+                                    let (k, b) = area;
+                                    if j < k {
+                                        a += b;
+                                    } else {
+                                        area_stack.push((k, b));
+                                        break;
+                                    };
+                                }
+                                _ => {
+                                    break;
+                                }
+                            };
+                        }
+
+                        area_stack.push((j, a));
+                    }
+                    _ => {}
+                };
+            }
+            _ => {}
+        }
+    }
+
+    println!("{}", sum);
+
+    print!("{} ", area_stack.len());
+    for i in 0..area_stack.len() {
+        let area = area_stack[i];
+        if i != area_stack.len() - 1 {
+            print!("{} ", area.1);
+        } else {
+            println!("{}", area.1);
+        }
+    }
 }
