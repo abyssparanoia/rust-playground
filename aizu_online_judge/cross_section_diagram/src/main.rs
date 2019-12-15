c2b25ed3f08977b53ea5d6b510ba71c4320f60c4
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-15 17:23:27 +0900

    faet: impl calculate

--- aizu_online_judge/cross_section_diagram/src/main.rs
+++ aizu_online_judge/cross_section_diagram/src/main.rs
@@ -11,8 +11,12 @@ fn read_line<T: FromStr>() -> Vec<T> {
 }
 
 struct Diagram {
-    width: i32,
-    depth: u32,
+    width: u32,
+    depth: i32,
+}
+
+const fn abs(v: i32) -> u32 {
+    [-v, v][(v >= 0) as usize] as u32
 }
 
 impl Diagram {
@@ -28,7 +32,7 @@ impl Diagram {
             }
             "/" => {
                 self.depth -= 1;
-                self.width -= 1;
+                self.width += 1;
             }
             _ => {}
         }
@@ -36,6 +40,10 @@ impl Diagram {
 
     fn is_finish(self) -> bool {
         self.depth == 0 && self.width != 0
+    }
+
+    fn caluclate(self) -> u32 {
+        (abs(self.depth) * self.width / 2) as u32
     }
 }
 
