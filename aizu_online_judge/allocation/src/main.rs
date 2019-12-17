b718e5db2138c059a590b115a8d3390c97ee97fc
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-18 03:34:17 +0900

    feat: impl check fn

--- aizu_online_judge/allocation/src/main.rs
+++ aizu_online_judge/allocation/src/main.rs
@@ -13,6 +13,25 @@ fn read<T: FromStr>() -> T {
     token.parse().ok().expect("failed to parse token")
 }
 
+fn check(w: Vec<u32>, n: usize, k: usize, p: u32) -> bool {
+    let mut idx: usize = 0;
+
+    for _i in 0..k {
+        let mut sum: u32 = 0;
+        while w[idx] + sum <= p {
+            sum += w[idx];
+            idx += 1;
+            if idx == n {
+                return true;
+            }
+        }
+    }
+    false
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n: usize = read();
+    let k: usize = read();
+
+    let w: Vec<u32> = (0..n).map(|_| read::<u32>()).collect();
 }
