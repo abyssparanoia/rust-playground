e0491bc690de3478ba9e4a5d303c31d55fbd03dd
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-19 02:11:30 +0900

    feat: impl logic

--- aizu_online_judge/merge_sort/src/main.rs
+++ aizu_online_judge/merge_sort/src/main.rs
@@ -13,15 +13,35 @@ fn read<T: FromStr>() -> T {
     token.parse().ok().expect("failed to parse token")
 }
 
+fn array_reader<T: FromStr>(n: usize) -> Vec<T> {
+    (0..n).map(|_| read::<T>()).collect()
+}
+
+fn array_display(array: Vec<u32>) {
+    let len = array.len() as usize;
+    for idx in 0..len {
+        if idx != len - 1 {
+            print!("{} ", array[idx]);
+        } else {
+            println!("{}", array[idx]);
+        }
+    }
+}
+
 fn merge(array: &mut Vec<u32>, left: usize, mid: usize, right: usize) {
     let n1 = mid - left;
     let n2 = right - mid;
 
+    // println!("{} {} {}", left, mid, right);
+    // println!("{} {}", n1, n2);
+
     let mut l_array: Vec<u32> = (0..n1).map(|idx| array[left + idx]).collect();
     let mut r_array: Vec<u32> = (0..n2).map(|idx| array[mid + idx]).collect();
 
-    l_array[n1] = std::u32::MAX;
-    r_array[n2] = std::u32::MAX;
+    // println!("{}", l_array.len());
+
+    l_array[n1 - 1] = std::u32::MAX;
+    r_array[n2 - 1] = std::u32::MAX;
 
     let mut idx_i: usize = 0;
     let mut idx_j: usize = 0;
@@ -37,15 +57,21 @@ fn merge(array: &mut Vec<u32>, left: usize, mid: usize, right: usize) {
     }
 }
 
-fn mergeSort(array: &mut Vec<u32>, left: usize, right: usize) {
+fn merge_sort(array: &mut Vec<u32>, left: usize, right: usize) {
     if left + 1 < right {
         let mid = (left + right) / 2;
-        mergeSort(array, left, mid);
-        mergeSort(array, mid, right);
+        merge_sort(array, left, mid);
+        merge_sort(array, mid, right);
         merge(array, left, mid, right);
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let n: usize = read();
+
+    let mut array = array_reader::<u32>(n);
+
+    merge_sort(&mut array, 0, n);
+
+    array_display(array);
 }
