f4ce479c05042b69cf1b1add8d6bb00979677223
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-23 01:06:24 +0900

    feat: impl logic

--- aizu_online_judge/the_number_of_inversions/src/main.rs
+++ aizu_online_judge/the_number_of_inversions/src/main.rs
@@ -13,18 +13,55 @@ fn read<T: FromStr>() -> T {
     token.parse().ok().expect("failed to parse token")
 }
 
-struct BIT {
-    array: Vec<u32>,
+fn array_reader<T: FromStr>(n: usize) -> Vec<T> {
+    (0..n).map(|_| read::<T>()).collect()
 }
 
-impl BIT {
-    fn new(size: usize) -> BIT {
-        BIT {
-            array: vec![0; size],
+fn merge(array: &mut Vec<u32>, left: usize, mid: usize, right: usize) -> usize {
+    let mut sum: usize = 0;
+
+    let n1 = mid - left;
+    let n2 = right - mid;
+    let mut l_array: Vec<u32> = (0..n1).map(|idx| array[left + idx]).collect();
+    let mut r_array: Vec<u32> = (0..n2).map(|idx| array[mid + idx]).collect();
+
+    l_array[n1 - 1] = std::u32::MAX;
+    r_array[n2 - 1] = std::u32::MAX;
+
+    let mut idx_i: usize = 0;
+    let mut idx_j: usize = 0;
+
+    for k in left..right {
+        if l_array[idx_i] <= r_array[idx_j] {
+            array[k] = l_array[idx_i];
+            idx_i += 1;
+        } else {
+            array[k] = r_array[idx_j];
+            idx_j += 1;
+            sum += 1;
         }
     }
+    sum
+}
+
+fn merge_sort(array: &mut Vec<u32>, left: usize, right: usize) -> usize {
+    let mut sum: usize = 0;
+    if left + 1 < right {
+        let mid = (left + right) / 2;
+        let sum_1 = merge_sort(array, left, mid);
+        let sum_2 = merge_sort(array, mid, right);
+        let sum_3 = merge(array, left, mid, right);
+        sum += sum_1 + sum_2 + sum_3;
+    }
+    sum
 }
 
 fn main() {
-    println!("Hello, world!");
+    let n: usize = read();
+
+    let mut array = array_reader::<u32>(n);
+
+    let result = merge_sort(&mut array, 0, n);
+
+    println!("{}", result);
 }
