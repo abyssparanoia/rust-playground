ada38d942c06e60de4374221627a6572db136c4c
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-28 22:32:37 +0900

    feat: impl logic

--- aizu_online_judge/counting_sort/src/main.rs
+++ aizu_online_judge/counting_sort/src/main.rs
@@ -13,6 +13,29 @@ fn read<T: FromStr>() -> T {
     token.parse().ok().expect("failed to parse token")
 }
 
+fn counting_sort(B: &mut Vec<u32>, n: usize, k: usize) {
+    let mut A = B.clone();
+
+    let mut C = vec![0; k];
+
+    for j in 1..n {
+        C[A[j] as usize] += 1;
+    }
+
+    for i in 1..k {
+        C[i] = C[i] + C[i - 1];
+    }
+
+    for j in n..1 {
+        B[C[A[j] as usize] as usize] = A[j];
+        C[A[j] as usize] -= 1;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n: usize = read();
+    let mut input_array: Vec<u32> = (0..n).map(|_| read::<u32>()).collect();
+    let max = *input_array.iter().max().unwrap() as usize + 1;
+
+    counting_sort(&mut input_array, n, max);
 }
