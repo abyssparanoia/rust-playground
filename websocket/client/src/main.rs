01b5a6d78ff9515cd25ec5c810bdd0b584d3367e
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-02-17 01:34:58 +0900

    feat: impl websocket client

--- websocket/client/src/main.rs
+++ websocket/client/src/main.rs
@@ -1,3 +1,13 @@
+use ws::{connect, CloseCode};
+
 fn main() {
-    println!("Hello, world!");
+    connect("ws://127.0.0.1:3012", |out| {
+        out.send("Hello WebSocket").unwrap();
+
+        move |msg| {
+            println!("Got message: {}", msg);
+            out.close(CloseCode::Normal)
+        }
+    })
+    .unwrap()
 }
